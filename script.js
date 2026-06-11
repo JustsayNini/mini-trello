ae2a212da7dbd3e864b27fbfbcdd2b2bf39131a5
JustsayNini <nissilossala@gmail.com>
2026-06-11 20:25:51 +0200

    Fixed a little more. A few more things are off... but if I touch anything else I will break it some more so here.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//Never
 let taskBoxes = JSON.parse(localStorage.getItem('taskBoxes')) || [];
 let addTask = document.getElementById("to-do");
 
 let taskBox;
+
+//gonna
 function displayCard(cardData){
 
     taskBox = document.createElement("div");
-    taskBox.className = "task-box-shell";
+    taskBox.className = "task-box-shell";//give
     taskBox.innerHTML = `
+        <div class="card-controls">
+            <button onclick="deleteCard(this)" class="delete-btn">Delete Card</button>
+            <button onclick="moveCard(this)" class="move-btn">Move Card</button>
+        </div>
         <h1 contenteditable="true" class="card-title" oninput="saveAllToStorage()">${cardData.title}</h1>
         <div class="checklist-container">
             <input type="checkbox" id="item-checkbox" ${cardData.checkbox ? 'checked' : ''} oninput="saveAllToStorage()">
@@ -16,9 +23,11 @@ function displayCard(cardData){
             <div class="list-container"></div>
         </div>
     `;
-    
+    //you
     addTask.appendChild(taskBox);
 }
+
+//THE CREATE PART OF THE CRUD OPERATIONS!
 
 function createCard() {
     const newCardData = {
@@ -31,9 +40,10 @@ function createCard() {
     
     localStorage.setItem('taskBoxes', JSON.stringify(taskBoxes));
     
-    displayCard(newCardData);
+    displayCard(newCardData);//up
 }
 
+//THE READ PART OF THE CRUD THINGY
 function loadSavedTasks() {
     addTask.innerHTML = ""; 
     
@@ -44,13 +54,14 @@ function loadSavedTasks() {
 
 loadSavedTasks();
 
+//ALSO THE CREATE PART OF THE CRUD OPERATIONS!!
 function saveTask(theclickedbutton) {
     let card = theclickedbutton.closest('.task-box-shell');
     
     let inputSpace = card.querySelector('#item-text');
     let checkboxSpace = card.querySelector('#item-checkbox');
     let listContainer = card.querySelector('.list-container');
-    
+    //never
     if (inputSpace.value.trim() === "") return;
     
     let newItem = document.createElement("p");
@@ -63,10 +74,11 @@ function saveTask(theclickedbutton) {
     
     listContainer.appendChild(newItem);
     
-    inputSpace.value = "";
+    inputSpace.value = "";//gonna
     checkboxSpace.checked = false;
 }
 
+//THE UPDATE PART OF THE CRUD OPERATIONSSS
 function saveAllToStorage() {
     const cardsOnScreen = document.querySelectorAll('.task-box-shell');
     let updatedList = [];
@@ -77,8 +89,45 @@ function saveAllToStorage() {
             checkbox: card.querySelector('.item-checkbox').checked,
             task: card.querySelector('.item-text').value
         });
-    });
+    });//let
 
     taskBoxes = updatedList;
     localStorage.setItem('taskBoxes', JSON.stringify(taskBoxes));
 }
+
+//THIS IS THE DELETE PART OF THE CRUD OPERATIONS MISS MA'AMS
+function deleteCard(buttonClicked) {
+    let card = buttonClicked.closest('.task-box-shell');
+    card.remove();
+}//you down
+
+
+function moveCard(buttonClicked) {
+    let card = buttonClicked.closest('.task-box-shell');
+    
+    let currentColumnId = card.parentElement.id;
+    
+    let doingColumn = document.getElementById('doing');
+    let doneColumn = document.getElementById('done');
+    let toDoColumn = document.getElementById('to-do');
+    
+    if (currentColumnId === "to-do") {
+        doingColumn.appendChild(card);
+    } else if (currentColumnId === "doing") {
+        doneColumn.appendChild(card); 
+    } else if (currentColumnId === "done") {
+        toDoColumn.appendChild(card);  
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//never gonna turn around and forget you~
